e2c62e01942a21501311735589421de41bd064a5
Ashkan.Zanjani <ashkan.zanjani@jambit.com>
2022-07-10 18:29:03 +0400

    continue

---
+++ app/src/main/java/com/dzm/app/dto/CompanyDto.java
@@ -0,0 +1,24 @@
+package com.dzm.app.dto;
+
+import com.dzm.app.domain.Company;
+import com.dzm.app.domain.Station;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CompanyDto {
+
+    @JsonProperty("parent_company_id")
+    private long parentCompanyId;
+    @JsonProperty("company_name")
+    private String companyName;
+    private Set<Station> stations;
+}
